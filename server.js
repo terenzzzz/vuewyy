ecad51a2ca9d9e4e8243e76660c84cafe7504738
terenzzzz <terence592592@gmail.com>
2022-06-19 11:53:52 +0100

    idk

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require('express');
+const path = require('path');
+const serverStatic = require('serve-static');
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-
-var port = process.env.PORT || 8000
-app.listen(port)
-console.log('server started ' + port)
+const app = express();
+app.use(serverStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(port);
