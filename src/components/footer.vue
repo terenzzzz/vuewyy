6aa8c38c6c9e3da61475de7c0aa5ad57dee33d73
terenzzzz <terence592592@gmail.com>
2022-06-21 23:44:07 +0100

    idk

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,40 +1,39 @@
 <template>
 	<!-- 页脚 -->
 	<div class="footer navbar fixed-bottom bg-light ">
-		<div class="container">
-			<div class="row align-items-center mt-2">
-				<div class="col-1"></div>
-				<div class="col-2">
-					<a href="#/" class="small text-decoration-none text-black">
-						<i class="fa-solid fa-globe display-6"></i>
-						<br>
-						<span>发现</span>
-					</a>
-				</div>
-				<div class="col-2">
-					<i class="fa-solid fa-headphones display-6"></i>
-					<span class="small">播客</span>
-				</div>
-				<div class="col-2">
-					<a href="#/myhome" class="small text-decoration-none text-black">
-						<i class="fa-solid fa-user-astronaut display-6 "></i>
-						<br>
-						<span>我的</span>
-					</a>
-				</div>
-				<div class="col-2">
-					<i class="fa-solid fa-user-group display-6"></i>
-					<span class="small">关注</span>
-				</div>
-				<div class="col-2">
-					<i class="fa-solid fa-house display-6"></i>
+		<div class="row align-items-center mt-2">
+			<div class="col-1"></div>
+			<div class="col-2">
+				<a href="#/" class="small text-decoration-none text-black">
+					<i class="fa-solid fa-globe display-6"></i>
 					<br>
-					<span>社区</span>
-				</div>
-				<div class="col-1"></div>
+					<span>发现</span>
+				</a>
 			</div>
-			<div class="mx-auto mb-0" ><p class="text-muted">@Terence{{year}}</p></div>
+			<div class="col-2">
+				<i class="fa-solid fa-headphones display-6"></i>
+				<span class="small">播客</span>
+			</div>
+			<div class="col-2">
+				<a href="#/myhome" class="small text-decoration-none text-black">
+					<i class="fa-solid fa-user-astronaut display-6 "></i>
+					<br>
+					<span>我的</span>
+				</a>
+			</div>
+			<div class="col-2">
+				<i class="fa-solid fa-user-group display-6"></i>
+				<span class="small">关注</span>
+			</div>
+			<div class="col-2">
+				<i class="fa-solid fa-house display-6"></i>
+				<br>
+				<span>社区</span>
+			</div>
+			<div class="col-1"></div>
 		</div>
+		<div class="mx-auto mb-0" ><p class="text-muted">@Terence{{year}}</p></div>
+
 	</div>
 
 </template>
